5dbe6c3408cd624aa8598551606892bda4ab0a3d
nihar-rs <niharranjan.2621@gmail.com>
2025-04-09 20:08:29 +0530

    PlayerAnimation-IDLE

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -9,11 +9,15 @@ import javax.swing.JPanel;
 
 import src.inputs.KeyboardInputs;
 import src.inputs.MouseInputs;
+import static src.utils.Constants.PlayerConstants.*;
 
 public class GamePanel extends JPanel {
   private MouseInputs mouseInputs = new MouseInputs(this);
   private float xDelta = 100, yDelta = 100;
-  private BufferedImage image, subImage;
+  private BufferedImage image;
+  private BufferedImage[][] animations;
+  private int animationTick, animationIndex, animationSpeed = 15;
+  private int playerAction = IDLE;
 
   public GamePanel() {
     addKeyListener(new KeyboardInputs(this) {
@@ -24,9 +28,20 @@ public class GamePanel extends JPanel {
 
     setPanelSize();
     importImage();
+    loadAnimations();
+  }
+
+  private void loadAnimations() {
+    animations = new BufferedImage[9][6];
+    for (int i = 0; i < animations.length; i++) {
+      for (int j = 0; j < animations[i].length; j++) {
+        animations[i][j] = image.getSubimage(j * 64, i * 40, 64, 40);
+      }
+    }
   }
 
   private void importImage() {
+
     try {
       image = ImageIO.read(getClass().getResource("../images/player_sprites.png"));
     } catch (Exception e) {
@@ -57,12 +72,24 @@ public class GamePanel extends JPanel {
     repaint();
   }
 
+  private void updateAnimationTick() {
+    animationTick++;
+    if (animationTick >= animationSpeed) {
+      animationTick = 0;
+      animationIndex++;
+      if (animationIndex >= getSpriteAmount(playerAction)) {
+        animationIndex = 0;
+      }
+    }
+  }
+
   public void paintComponent(Graphics graphics) {
     super.paintComponent(graphics);
 
+    updateAnimationTick();
     if (image != null) {
-      subImage = image.getSubimage(1 * 64, 8 * 40, 64, 40);
-      graphics.drawImage(subImage, (int) xDelta, (int) yDelta, 128, 80, null);
+      graphics.drawImage(animations[playerAction][animationIndex], (int) xDelta, (int) yDelta, 128, 80, null);
     }
   }
+
 }
